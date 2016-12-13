3daf0bafcb1d1c1c2f61bbd11ad001b0e9bedee5
Aditya <Aditya>
2016-12-14 01:29:28 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+var app = express();
+const fs =require('fs');
+const hbs = require('hbs');
+
+app.set('view engine', 'hbs');
+
+hbs.registerPartials(__dirname + '/views/partials')
+
+app.use(function (req, res, next){
+  var now = new Date().toString();
+  var log = `${now}:  ${req.method} ${req.url}`;
+  //console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
+});
+
+app.use(function(req, res, next){
+  res.render('main.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getcurrentYear', function(){
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', function(text){
+  return text.toUpperCase();
+})
+
+app.get('/',function(req, res){
+  res.render('home.hbs', {
+    pageTitle : "It's the HOMEPAGE",
+    welcomeMessage : "Welcome!!!"
+    });
+});
+
+app.get('/help', function(req, res){
+  res.render('help.hbs',{
+    pageTitle: "It's the Help Page"
+  });
+});
+
+app.get('/about', function(req, res){
+  res.render('about.hbs', {
+    pageTitle : 'About PAGE'  });
+  });
+
+app.get('/error',function(req, res){
+  res.send("<h1>This is an ERROR</h1>");
+});
+
+app.listen(3000, function(){
+  console.log("The server is running...");
+});
